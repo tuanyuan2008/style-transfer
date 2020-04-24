009af08c2c08b08f6edffa027eb40fdefafaa5f1
Sarah Chen <schen1337@gmail.com>
2020-04-23 22:29:38 -0400

    processed text

--- data/process.py
+++ data/process.py
@@ -1,6 +1,31 @@
 import re
 
-with open('dev/en.txt', 'r') as f, open('dev/en_2.txt', 'w') as g:
-    line = re.sub(r'someword=|\,.*|\#.*','', f.read())
-    line = re.sub(r'\n+', '\n', line).strip()
-    g.writelines(line)
+with open('processed_files/dev/en.txt', 'r') as f, open('processed_files/dev/en_2.txt', 'w') as g:
+    # line = re.sub(r'someword=|\,.*|\#.*','', f.read())
+    # line = re.sub(r'\n+', '\n', line).strip()
+    # g.writelines(line)
+    for line in f:
+        if line != '<POS> <CON_START>  <START>  <END>\n':
+            g.write(line)
+f.close()
+g.close()
+
+with open('processed_files/test/en.txt', 'r') as f, open('processed_files/test/en_2.txt', 'w') as g:
+    # line = re.sub(r'someword=|\,.*|\#.*','', f.read())
+    # line = re.sub(r'\n+', '\n', line).strip()
+    # g.writelines(line)
+    for line in f:
+        if line != '<POS> <CON_START>  <START>  <END>\n':
+            g.write(line)
+f.close()
+g.close()
+
+with open('processed_files/train/en.txt', 'r') as f, open('processed_files/train/en_2.txt', 'w') as g:
+    # line = re.sub(r'someword=|\,.*|\#.*','', f.read())
+    # line = re.sub(r'\n+', '\n', line).strip()
+    # g.writelines(line)
+    for line in f:
+        if line != '<POS> <CON_START>  <START>  <END>\n':
+            g.write(line)
+f.close()
+g.close()
