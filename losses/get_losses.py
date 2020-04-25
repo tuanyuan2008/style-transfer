6769f85c1a0d689b602cbdff6994958b19a21a20
Sarah Chen <schen1337@gmail.com>
2020-04-24 21:04:37 -0400

    losses

---
+++ losses/get_losses.py
@@ -0,0 +1,23 @@
+import pickle
+
+losses = []
+with open('dg_losses.txt', 'r') as f:
+    for line in f:
+        losses.append(float(line[15:23]))
+f.close()
+
+with open('dg_losses.data', 'wb') as g:
+    # store the data as binary data stream
+    pickle.dump(losses, g)
+g.close()
+
+losses = []
+with open('drg_losses.txt', 'r') as f:
+    for line in f:
+        losses.append(float(line[15:23]))
+f.close()
+
+with open('drg_losses.data', 'wb') as g:
+    # store the data as binary data stream
+    pickle.dump(losses, g)
+g.close()
---
+++ losses/plot.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import pickle
+
+# with open('dg_losses.data', 'rb') as f:
+#     # read the data as binary data stream
+#     dg_losses = pickle.load(f)
+#     plt.title('Delete-Generate Model Losses Per Iteration')
+#     plt.xlabel('Iteration')
+#     plt.ylabel('Loss')
+#     plt.plot(dg_losses)
+#     plt.savefig('dg_loss.png')
+
+# f.close()
+
+with open('drg_losses.data', 'rb') as f:
+    # read the data as binary data stream
+    dg_losses = pickle.load(f)
+    plt.title('Delete-Retrieve-Generate Model Losses Per Iteration')
+    plt.xlabel('Iteration')
+    plt.ylabel('Loss')
+    plt.plot(dg_losses)
+    plt.savefig('drg_loss.png')
+
+f.close()
